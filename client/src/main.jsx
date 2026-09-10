03806d25c8be9e43cbf0749282a990ca491f8d53
Esdras Jimenez <esdrasj71@gmail.com>
2026-09-10 02:18:14 -0400

    Changing basename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY){
 }
 
 createRoot(document.getElementById('root')).render(
-     <BrowserRouter basename="/Project-Management-Platform">
+     <BrowserRouter>
          <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
             <Provider store={store}>
                 <App />
